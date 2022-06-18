07f3ecc223043c3f68db0081585ac8142eb7ebc8
Simon Larsén <slarse@slar.se>
2022-06-18 13:24:24 +0200

    Replace repobee-junit4 with Rusthon in repos table

--- update.py
+++ update.py
@@ -15,7 +15,7 @@ TEMPLATE_PATH = CUR_DIR / "README_TEMPLATE.md"
 FEED_URL = "https://slar.se/feeds/all.atom.xml"
 NUM_POSTS = 5
 
-REPOS = "repobee/repobee SpoonLabs/sorald inria/spoon repobee/repobee-junit4 KTH/spork slarse/pygitviz".split()
+REPOS = "repobee/repobee inria/spoon KTH/spork slarse/rusthon slarse/pygitviz SpoonLabs/sorald".split()
 USER = "slarse"
 
 
